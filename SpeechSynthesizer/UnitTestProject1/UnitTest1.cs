2f78b6fcb101ecd4820e013ec5cccbf0582dab0b
Brien <Brien.Developer@gmail.com>
2017-11-19 18:53:04 -0800

    Commit existing project

---
+++ SpeechSynthesizer/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string startupText = "Welcome to the speech synthesizer. Please enter text and press enter to hear me speak what you write:";
+            SpeechSynth.Main();
+            Assert.AreEqual(Console.Out.ToString(), startupText);
+            Environment.Exit(0);
+        }
+    }
+}
